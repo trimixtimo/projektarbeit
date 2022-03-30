cabf28e0d735ee67d1dd509cfa002961c0a54080
Timo Klaas <timo.klaas@cardis.io>
2022-03-30 17:14:09 +0200

    Testprogramm blinkende LED fertiggestellt

--- Firmware/Testfirmware_LED/Testfirmware_LED/main.c
+++ Firmware/Testfirmware_LED/Testfirmware_LED/main.c
@@ -26,27 +26,40 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 
-int setup_cpu(void)
+void setup_cpu(void)
 {
-	//CCP setzen!
-	CLKCTRL_MCLKCTRLA = 0b00000000;	//Disable Clock-Out, interne Clock
-	CLKCTRL_MCLKCTRLB = (0x8 <<1) | (0x01);	//Prescaler 6x, Prescaler an -> 4MHz CLK Main
+	CCP = CCP_IOREG_gc;	//Schreibschutz aufheben
+	CLKCTRL_MCLKCTRLA |= 0b00000000;	//Disable Clock-Out, interne Clock
+	CLKCTRL_MCLKCTRLB |= (0x8 <<1) | (0x01);	//Prescaler 6x, Prescaler an -> 4MHz CLK Main
 }
-int setup_io(void)
+void setup_io(void)
 {
-	PORTA_DIR = 01011001;	//SPI Interface Input/Output
-	PORTC_DIR = 00000000;
-	PORTD_DIR = 01100000;	//LED Outputs
-	PORTC_PIN0CTRL = 1 << 3;	//Pullup für Button
+	PORTA_DIR |= 0b01011001;	//SPI Interface Input/Output
+	PORTC_DIR |= 0b00000000;
+	PORTD_DIR |= 0b01100000;	//LED Outputs
+	PORTC_PIN0CTRL |= 1 << 3;	//Pullup für Button
+}
+void setup_vref(void)
+{
+	VREF_ADC0REF = VREF_ALWAYSON_bm | VREF_REFSEL_VREFA_gc;	//Referenzspannung immer an, externe Referenzspannung
 }
 
 
 int main(void)
 {
-    /* Replace with your application code */
+	void setup_cpu();
+	void setup_io();
+	
     while (1) 
     {
-		
+		PORTD_OUTSET = 0b00100000;
+		_delay_ms(500);
+		PORTD_OUTCLR = 0b00100000;
+		_delay_ms(500);
+		PORTD_OUTSET = 0b01000000;
+		_delay_ms(500);
+		PORTD_OUTCLR = 0b01000000;
+		_delay_ms(500);
     }
 }
 
