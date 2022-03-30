37c12582324626bb051a2cf7a5472988397fb6b5
xmxxnu <meenujan.sritharan@stud.hs-ruhrwest.de>
2022-03-30 12:25:43 +0200

    Update Verarbeitung.m

--- Matlab/Verarbeitung.m
+++ Matlab/Verarbeitung.m
@@ -1,6 +1,6 @@
 filename = 'Messung0-2-0-1-0.txt'; % Dateinamen als Variable definieren
-FID = fopen(filename);
+FID = fopen(filename); % Datei in Matlab öffnen 
 dataFromfile = textscan(FID, '%s');% Die Werte als String lesen für HEX Verarbeitung
 dataFromfile = dataFromfile{1};
-decData = hex2dec(dataFromfile);
+decData = hex2dec(dataFromfile); % Hexadezimal in Dezimal umwandeln
 fclose(FID); % geöffnete Datei in Matlab schließen
