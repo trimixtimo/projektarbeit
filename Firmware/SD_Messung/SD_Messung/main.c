0669360535b8bf80f2975ef1fb106b7553b8ecb5
Timo <timok95@gmail.com>
2022-04-01 22:46:17 +0200

    Neues Projekt für Hauptfirmware, LED Firmware auf notwendigsten Inhalt reduziert

--- Firmware/SD_Messung/SD_Messung/main.c
+++ Firmware/SD_Messung/SD_Messung/main.c
@@ -1,8 +1,8 @@
 /*
- * Testfirmware_LED.c
+ * SD_Messung.c
  *
- * Created: 30.03.2022 11:53:45
- * Author : Timo Klaas
+ * Created: 01.04.2022 22:42:57
+ * Author : Timo
  */ 
 
 /*
@@ -35,6 +35,35 @@ void setup_io(void)
 	PORTD_DIR = 0b01100000;	//LED Outputs
 	PORTC_PIN0CTRL = (1 << 3);	//Pullup für Button
 }
+void setup_vref(void)
+{
+	VREF_ADC0REF = VREF_ALWAYSON_bm | VREF_REFSEL_VREFA_gc;	//Referenzspannung immer an, externe Referenzspannung
+}
+void setup_adc(void)
+{
+	ADC0_CTRLA |= (ADC_RUNSTBY_bm | ADC_FREERUN_bm);
+	ADC0_CTRLC |= ADC_PRESC_DIV256_gc;	//niedriger Takt für maximale Auflösung: 4 MHz / 256 = 15 kHz
+	ADC0_MUXPOS = 0x01; //für RAW-Input
+	//ADC0_MUXPOS = 0x02; //für HULL-Input
+	ADC0_DBGCTRL |= ADC_DBGRUN_bm;	//ADC debugging
+	ADC0_CTRLA |= ADC_ENABLE_bm;	//ADC einschalten
+	_delay_ms(1);
+	ADC0_COMMAND |= ADC_STCONV_bm;	//erste Messung starten
+}
+void setup_timer(void)
+{
+	TCA0_SINGLE_PER = periodendauer_us;
+	TCA0_SINGLE_CTRLA = (TCA_SINGLE_RUNSTDBY_bm | (0x02 << 1) | TCA_SINGLE_ENABLE_bm);	//Prescaler 4: 4 MHz / 4 = 1 MHz
+	TCA0_SINGLE_DBGCTRL = 0x01;	//debugging
+	TCA0_SINGLE_INTCTRL = TCA_SINGLE_OVF_bm;	//Überlauf-Interrupt aktiviert
+	
+	//TCA0_SINGLE_INTFLAGS = 1; muss in der ISR aufgerufen werden
+	
+}
+uint16_t adc_read(void)
+{
+	return ADC0_RES;
+}
 
 int main(void)
 {
@@ -42,7 +71,7 @@ int main(void)
 	setup_io();
 	
 	while(1)
-	{	
+	{
 		if(!(PORTC_IN & 0x01))
 		{
 			PORTD_OUTSET = 0b00100000;
@@ -65,4 +94,3 @@ int main(void)
 		}
 	}
 }
-
