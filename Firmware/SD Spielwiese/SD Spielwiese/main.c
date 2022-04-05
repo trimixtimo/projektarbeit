27f34cb05915d7639e189f8f109e25887d2fd51d
Timo <timok95@gmail.com>
2022-04-05 08:54:36 +0200

    SD Spielwiese eingerichtet

---
+++ Firmware/SD Spielwiese/SD Spielwiese/main.c
@@ -0,0 +1,18 @@
+/*
+ * SD Spielwiese.c
+ *
+ * Created: 05.04.2022 08:54:09
+ * Author : Timo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
